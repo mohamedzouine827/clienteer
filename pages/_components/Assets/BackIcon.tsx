a79aab095ef28d2faa9d28486206bb3eb53c38b3
mohamedzouine827 <mohamedzouine82o@gmail.com>
2024-08-04 03:17:14 -0700

    Components UI

---
+++ pages/_components/Assets/BackIcon.tsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+const BackIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={40}
+    height={40}
+    fill="none"
+  >
+    <path
+      fill="#0F0F0F"
+      d="M26.422 7.711a1.949 1.949 0 0 0-2.756 0l-9.526 9.535a3.898 3.898 0 0 0 .002 5.512l9.53 9.53a1.949 1.949 0 1 0 2.757-2.755l-8.158-8.158a1.949 1.949 0 0 1 0-2.756l8.151-8.151a1.949 1.949 0 0 0 0-2.757Z"
+    />
+  </svg>
+)
+export default BackIcon
